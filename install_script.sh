f7679e9bb0c3cdb5fda14477c9ae93ba10650275
kyledong <dong.jing@racochina.com>
2020-02-05 21:25:15 +0800

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -126,12 +126,12 @@ cp crl.pem /etc/ocserv/
 #配置 ocserv
 cd /etc/ocserv/
 rm -rf ocserv.conf
-wget https://raw.githubusercontent.com/chendong12/ocserv/master/ocserv.conf
+wget https://github.com/kyledong/ocserv/blob/master/ocserv.conf
 #
 cd /root/anyconnect
-wget https://raw.githubusercontent.com/chendong12/ocserv/master/gen-client-cert.sh
-wget https://raw.githubusercontent.com/chendong12/ocserv/master/user_add.sh
-wget https://raw.githubusercontent.com/chendong12/ocserv/master/user_del.sh
+wget https://github.com/kyledong/ocserv/blob/master/gen-client-cert.sh
+wget https://github.com/kyledong/ocserv/blob/master/user_add.sh
+wget https://github.com/kyledong/ocserv/blob/master/user_del.sh
 chmod +x gen-client-cert.sh
 chmod +x user_add.sh
 chmod +x user_del.sh
